5c30bfe99a65b3bd37c35bb5582f1d144c6e0ba3
Richard <richardsidharta3@gmail.com>
2024-12-14 16:04:46 +0700

    init

---
+++ app/src/main/java/com/example/projectuas/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.projectuas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.projectuas.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun addFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.)
+            .replace
+    }
+}
